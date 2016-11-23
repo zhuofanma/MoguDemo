2acc899b9918d028ea3b62bc0cc389f86ed038ea
Joe <fdu.zhuofan.ma@gmail.com>
2016-11-23 18:43:46 +0800

    Initial Commit

---
+++ MoguDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoguDemo
+//
+//  Created by Zhuofan Ma on 11/23/16.
+//  Copyright © 2016 Zhuofan Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
